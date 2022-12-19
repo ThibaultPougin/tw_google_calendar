8c9fd591e00f59972ba22a15ab0a067eef00a665
ThibaultPougin <t.pougin@gmail.com>
2022-12-19 18:33:00 +0100

    Ajout CLI interactif

---
+++ src/controllers/cli_controller.js
@@ -0,0 +1,154 @@
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+
+const teamwork_controller = require('./teamwork_controller');
+
+const cli_controller = {
+
+    handle_interactive_command_line: async (total_log_hours, total_unlog_hours, unlog_events) => {
+
+        console.log(`- Nombres d'heures synchronisées : ${total_log_hours}`);
+        console.log(`- Nombres d'heures non synchronisées : ${total_unlog_hours}`);
+
+        let questions = [];
+
+        for (const event of unlog_events) {
+            let question = await cli_controller.create_question(event);
+            question.forEach((q) => {
+                questions.push(q);
+            });
+        };
+     
+        inquirer.prompt(questions)
+        .then(answers => {
+
+            let event_to_add = [];
+
+            for (const question in answers) {
+
+                if(question.includes('Question 2') && answers[question] === 'Support') {
+                    event_to_add.push({event_id: question.split(' ').pop(), teamwork_id: process.env.TEAMWORK_SUPPORT_REPOSITORY_ID});
+                };
+
+                if(question.includes('Question 2') && answers[question] === 'Réunion') {
+                    event_to_add.push({event_id: question.split(' ').pop(), teamwork_id: process.env.TEAMWORK_REUNION_REPOSITORY_ID});
+                };
+
+                if(question.includes('Question 2') && answers[question] === 'Gestion de projet') {
+                    event_to_add.push({event_id: question.split(' ').pop(), teamwork_id: process.env.TEAMWORK_PROJECT_REPOSITORY_ID});
+                };
+
+                if(question.includes('Question 2') && answers[question] === 'Formation') {
+                    event_to_add.push({event_id: question.split(' ').pop(), teamwork_id: process.env.TEAMWORK_FORMATION_REPOSITORY_ID});
+                };
+
+                if(question.includes('Question 3')) {
+                    event_to_add.push({event_id: question.split(' ').pop(), teamwork_id: answers[question]});
+                };               
+            };
+
+            cli_controller.add_events(total_log_hours, unlog_events, event_to_add);
+
+        });
+    },
+
+    add_events: async (total_log_hours, unlog_events, event_to_add) => {
+
+        total_log_minutes = 0;
+
+        for (const event of event_to_add) {
+            let find_event = unlog_events.find(e => e.id === event.event_id)
+
+            let split_event = find_event.summary.split('/');
+            let start_date = find_event.start.dateTime.substring(0, 10);
+            let start_time = find_event.start.dateTime.substring(11, 19);
+            let start_hour = find_event.start.dateTime.substring(11, 13);
+            let end_time = find_event.end.dateTime.substring(11, 19);
+            let end_hour = find_event.end.dateTime.substring(11, 13);
+            let start_hour_to_number = parseInt(start_hour, 10);
+            let end_hour_to_number = parseInt(end_hour, 10);
+
+            let hours = end_hour_to_number - start_hour_to_number - 1;
+
+            let start_minute = find_event.start.dateTime.substring(14, 16);
+            let end_minute = find_event.end.dateTime.substring(14, 16);
+            let start_minute_to_number = parseInt(start_minute, 10);
+            let end_minute_to_number = parseInt(end_minute, 10);
+
+            let minutes = end_minute_to_number + (60 - start_minute_to_number);
+
+            if(minutes >= 60) {
+                minutes = minutes - 60;
+                hours++;
+            };
+
+            teamwork_controller.log_time_teamwork(event.teamwork_id, start_date, start_time, hours, minutes);
+
+            total_log_hours = total_log_hours + hours;
+            total_log_minutes = total_log_minutes + minutes;
+
+            if (total_log_minutes >= 60) {
+                total_log_minutes = total_log_minutes - 60;
+                total_log_hours++;
+            };
+
+        };
+
+        console.log(chalk.green(`Evenements synchronisés ! Nombre d'heures totales synchronisées : ${total_log_hours} heures.`));
+
+    },
+
+    create_question: async (event) => {
+
+        if(event.start && event.start.dateTime && event.end && event.end.dateTime) {
+
+            let split_event = event.summary.split('/');
+            let start_date = event.start.dateTime.substring(0, 10);
+            let start_time = event.start.dateTime.substring(11, 19);
+            let start_hour = event.start.dateTime.substring(11, 13);
+            let end_time = event.end.dateTime.substring(11, 19);
+            let end_hour = event.end.dateTime.substring(11, 13);
+            let start_hour_to_number = parseInt(start_hour, 10);
+            let end_hour_to_number = parseInt(end_hour, 10);
+
+            let hours = end_hour_to_number - start_hour_to_number - 1;
+
+            let start_minute = event.start.dateTime.substring(14, 16);
+            let end_minute = event.end.dateTime.substring(14, 16);
+            let start_minute_to_number = parseInt(start_minute, 10);
+            let end_minute_to_number = parseInt(end_minute, 10);
+
+            let minutes = end_minute_to_number + (60 - start_minute_to_number);
+
+            if(minutes >= 60) {
+                minutes = minutes - 60;
+                hours++;
+            };
+
+            let question = [{
+                type: 'list',
+                name: 'Question 1 ' + event.id,
+                message: `Souhaitez-vous synchroniser cet événement : ${event.summary} du ${start_date} de ${start_time} à ${end_time} (${hours} heures ${minutes} minutes) ?`,
+                choices: ['Oui', 'Non'],
+            }, {
+                type: 'list',
+                name: 'Question 2 ' + event.id,
+                message: `Quel est le type de l'événement ?`,
+                choices: ['Support', 'Réunion', 'Gestion de projet', 'Formation', 'Autre'],
+                when: (answers) => answers['Question 1 ' + event.id] === 'Oui'
+            },
+            {
+                type: 'input',
+                name: 'Question 3 ' + event.id,
+                message: `Indiquez l'ID de la tâche Teamwork :`,
+                when: (answers) => answers['Question 2 ' + event.id] === 'Autre'
+            }];
+    
+            return question;
+
+        }        
+    }
+
+}
+
+module.exports = cli_controller;
--- src/controllers/google_agenda_controller.js
+++ src/controllers/google_agenda_controller.js
@@ -1,5 +1,7 @@
 const { google } = require('googleapis');
+
 const teamwork_controller = require('./teamwork_controller');
+const cli_controller = require('./cli_controller');
 
 let google_client_id = process.env.GOOGLE_CLIENT_ID;
 let google_client_secret = process.env.GOOGLE_CLIENT_SECRET;
@@ -52,68 +54,116 @@ const google_agenda_controller = {
         return friday.toISOString().split('T')[0];
     },
 
+    get_teamwork_task_id: (event) => {
+
+        let teamwork_id = undefined; 
+      
+        if(event.summary && event.summary.includes('https://helliosolutions.teamwork.com')) {
+            const task = 'tasks/';
+            const index = event.summary.indexOf(task);
+            const length = task.length;
+            const string1 = event.summary.slice(index + length);
+            teamwork_id = string1.match(/[0-9]+/)[0];
+        } else if (event.summary && !event.summary.includes('https://helliosolutions.teamwork.com') && (event.summary.includes('Daily meeting') || event.summary.includes('Weekly Dev - Point Projets') || event.summary.includes('Attribution des US') || event.summary.includes('Sprint') || event.summary.includes('Temps TW'))) {
+            teamwork_id = process.env.TEAMWORK_PROJECT_REPOSITORY_ID;    
+        } else if (event.summary && !event.summary.includes('https://helliosolutions.teamwork.com') && event.summary.includes('Formation')) {
+            teamwork_id = process.env.TEAMWORK_FORMATION_REPOSITORY_ID;
+        } else if(event.summary && !event.summary.includes('https://helliosolutions.teamwork.com') && event.summary.includes('Support') || event.summary.includes('AH')) {
+            teamwork_id = process.env.TEAMWORK_SUPPORT_REPOSITORY_ID;   
+        } else if(event.attendees) {
+            teamwork_id = process.env.TEAMWORK_REUNION_REPOSITORY_ID;   
+        };
+
+        return teamwork_id;
+    },
+
     start: async () => {
 
+        // On récupère la date de début (lundi matin) et date de fin (vendredi soir) de la semaine courante
         const start = google_agenda_controller.getMonday(new Date()) + 'T00:00:00+01:00';
         const end = google_agenda_controller.getFriday(new Date()) + 'T23:00:00+01:00';
         
+        // On récupère tous les événements de la semaine renseignés dans l'agenda
         const events = await google_agenda_controller.getEvents(start, end);
 
         let total_log_hours = 0;
+        let total_log_minutes = 0;
+        let total_unlog_hours = 0;
+        let total_unlog_minutes = 0;
+
+        let unlog_events = [];
     
         events.forEach(event => {
 
+            // Si l'événement a une date de début et une date de fin (exclu les événements sur une journée entière comme les congés)
             if(event.start && event.start.dateTime && event.end && event.end.dateTime) {
                 
-                let split_event = event.summary.split('/');
-                let start_date = event.start.dateTime.substring(0, 10);
-                let start_time = event.start.dateTime.substring(11, 19);
-                let start_hour = event.start.dateTime.substring(11, 13);
-                let end_hour = event.end.dateTime.substring(11, 13);
-                let start_hour_to_number = parseInt(start_hour, 10);
-                let end_hour_to_number = parseInt(end_hour, 10);
-
-                let hours = end_hour_to_number - start_hour_to_number - 1;
-
-                let start_minute = event.start.dateTime.substring(14, 16);
-                let end_minute = event.end.dateTime.substring(14, 16);
-                let start_minute_to_number = parseInt(start_minute, 10);
-                let end_minute_to_number = parseInt(end_minute, 10);
-
-                let minutes = end_minute_to_number + (60 - start_minute_to_number);
-
-                if(minutes >= 60) {
-                    minutes = minutes - 60;
-                    hours++;
-                };
-
-                let teamwork_id = undefined;
-        
-                if(event.summary && event.summary.includes('https://helliosolutions.teamwork.com')) {
-                    const task = 'tasks/';
-                    const index = event.summary.indexOf(task);
-                    const length = task.length;
-                    const string1 = event.summary.slice(index + length);
-                    teamwork_id = string1.match(/[0-9]+/)[0];
-                } else if (event.summary && !event.summary.includes('https://helliosolutions.teamwork.com') && (event.summary.includes('Daily meeting') || event.summary.includes('Weekly Dev - Point Projets') || event.summary.includes('Attribution des US') || event.summary.includes('Sprint') || event.summary.includes('Temps TW'))) {
-                    teamwork_id = process.env.TEAMWORK_PROJECT_REPOSITORY_ID;    
-                } else if (event.summary && !event.summary.includes('https://helliosolutions.teamwork.com') && event.summary.includes('Formation')) {
-                    teamwork_id = process.env.TEAMWORK_FORMATION_REPOSITORY_ID;
-                } else if(event.summary && !event.summary.includes('https://helliosolutions.teamwork.com') && event.summary.includes('Support')) {
-                    teamwork_id = process.env.TEAMWORK_SUPPORT_REPOSITORY_ID;   
-                } else if(event.attendees) {
-                    teamwork_id = process.env.TEAMWORK_REUNION_REPOSITORY_ID;   
-                };
+                let teamwork_id = google_agenda_controller.get_teamwork_task_id(event);
 
                 if(teamwork_id !== undefined) {
+
+                    let split_event = event.summary.split('/');
+                    let start_date = event.start.dateTime.substring(0, 10);
+                    let start_time = event.start.dateTime.substring(11, 19);
+                    let start_hour = event.start.dateTime.substring(11, 13);
+                    let end_hour = event.end.dateTime.substring(11, 13);
+                    let start_hour_to_number = parseInt(start_hour, 10);
+                    let end_hour_to_number = parseInt(end_hour, 10);
+
+                    let hours = end_hour_to_number - start_hour_to_number - 1;
+
+                    let start_minute = event.start.dateTime.substring(14, 16);
+                    let end_minute = event.end.dateTime.substring(14, 16);
+                    let start_minute_to_number = parseInt(start_minute, 10);
+                    let end_minute_to_number = parseInt(end_minute, 10);
+
+                    let minutes = end_minute_to_number + (60 - start_minute_to_number);
+
+                    if(minutes >= 60) {
+                        minutes = minutes - 60;
+                        hours++;
+                    };
+                   
+                    // teamwork_controller.log_time_teamwork(teamwork_id, start_date, start_time, hours, minutes);
+
                     total_log_hours = total_log_hours + hours;
-                    teamwork_controller.log_time_teamwork(teamwork_id, start_date, start_time, hours, minutes);
+                    total_log_minutes = total_log_minutes + minutes;
+
+                    if (total_log_minutes >= 60) {
+                        total_log_minutes = total_log_minutes - 60;
+                        total_log_hours++;
+                    };
+                } else {
+                    unlog_events.push(event);
+
+                    let start_hour = event.start.dateTime.substring(11, 13);
+                    let end_hour = event.end.dateTime.substring(11, 13);
+                    let start_hour_to_number = parseInt(start_hour, 10);
+                    let end_hour_to_number = parseInt(end_hour, 10);
+
+                    let hours = end_hour_to_number - start_hour_to_number - 1;
+
+                    let start_minute = event.start.dateTime.substring(14, 16);
+                    let end_minute = event.end.dateTime.substring(14, 16);
+                    let start_minute_to_number = parseInt(start_minute, 10);
+                    let end_minute_to_number = parseInt(end_minute, 10);
+
+                    let minutes = end_minute_to_number + (60 - start_minute_to_number);
+
+                    if(minutes >= 60) {
+                        minutes = minutes - 60;
+                        hours++;
+                    };
+
+                    total_unlog_hours = total_unlog_hours + hours;
+                    total_unlog_minutes = total_unlog_minutes + minutes;
                 };
             };
 
         });
 
-        console.log(`Nombres d'heures ajoutées total : ${total_log_hours} / 40`);
+        cli_controller.handle_interactive_command_line(total_log_hours, total_unlog_hours, unlog_events);
+
     }    
 };
 
